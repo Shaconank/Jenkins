bab6db91cb11985275e64a65d1790d59ec8759dd
Shaconank <shashankcanchi42@gmail.com>
2025-03-18 15:53:17 +0530

    ta slander

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "This is changed Now\n";
+  cout << "Ta should use 2 braincell";
   return 0;
 }
